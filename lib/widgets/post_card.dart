777d425204a38d0e26e19c74ecc48c71583efdea
Zumazoy <solovevstepan878@gmail.com>
2026-02-12 11:37:46 +0300

    Added animation to the post

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,99 +1,228 @@
 import 'package:flutter/material.dart';
 import '../models/post.dart';
 
-class PostCard extends StatelessWidget {
+class PostCard extends StatefulWidget {
   final Post post;
 
   const PostCard({super.key, required this.post});
 
   @override
+  State<PostCard> createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> with SingleTickerProviderStateMixin {
+  bool _isExpanded = false;
+  late AnimationController _animationController;
+  late Animation<double> _heightAnimation;
+  late Animation<Color?> _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+
+    _heightAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+
+    _colorAnimation = ColorTween(
+      begin: Colors.white,
+      end: Colors.blue.shade50,
+    ).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _toggleExpand() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded) {
+        _animationController.forward();
+      } else {
+        _animationController.reverse();
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      elevation: 2,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.blue[100],
-                  child: Text(
-                    post.userId.toString(),
-                    style: const TextStyle(color: Colors.blue),
-                  ),
-                ),
-                const SizedBox(width: 12),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+    return GestureDetector(
+      onTap: _toggleExpand,
+      child: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) {
+          return Card(
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            elevation: 2,
+            color: _colorAnimation.value,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Верхняя часть с аватаром и информацией
+                  Row(
                     children: [
-                      Text(
-                        'User ${post.userId}',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
+                      CircleAvatar(
+                        backgroundColor: Colors.blue[100],
+                        child: Text(
+                          widget.post.userId.toString(),
+                          style: const TextStyle(color: Colors.blue),
                         ),
                       ),
+                      const SizedBox(width: 12),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'User ${widget.post.userId}',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                            Text(
+                              _formatDate(widget.post.date),
+                              style: TextStyle(
+                                color: Colors.grey[600],
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Icon(
+                        _isExpanded ? Icons.expand_less : Icons.expand_more,
+                        color: Colors.grey[600],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 12),
+                  
+                  // Заголовок (всегда виден)
+                  Text(
+                    widget.post.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                    maxLines: _isExpanded ? null : 2,
+                    overflow: _isExpanded ? TextOverflow.visible : TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8),
+                  
+                  // Анимированная часть контента
+                  ClipRect(
+                    child: Align(
+                      heightFactor: _heightAnimation.value,
+                      child: Column(
+                        children: [
+                          // Полный текст поста
+                          Text(
+                            widget.post.body,
+                            style: TextStyle(
+                              color: Colors.grey[700],
+                              fontSize: 14,
+                              height: 1.5,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          
+                          // Дополнительная информация, которая появляется при расширении
+                          Container(
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    const Icon(Icons.person_outline, size: 16),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      'Author ID: ${widget.post.userId}',
+                                      style: TextStyle(color: Colors.grey[800]),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 8),
+                                Row(
+                                  children: [
+                                    const Icon(Icons.tag, size: 16),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      'Post #${widget.post.id}',
+                                      style: TextStyle(color: Colors.grey[800]),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 8),
+                                Row(
+                                  children: [
+                                    const Icon(Icons.calendar_today, size: 16),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      'Published: ${_formatDate(widget.post.date)}',
+                                      style: TextStyle(color: Colors.grey[800]),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  
+                  const SizedBox(height: 12),
+                  
+                  // Нижняя панель с лайками и номером поста
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            _isExpanded ? Icons.favorite : Icons.favorite_border,
+                            size: 20,
+                            color: _isExpanded ? Colors.red[400] : Colors.grey[600],
+                          ),
+                          const SizedBox(width: 4),
+                          Text(
+                            '${(widget.post.id * 3) % 100}',
+                            style: TextStyle(
+                              color: _isExpanded ? Colors.red[400] : Colors.grey[600],
+                            ),
+                          ),
+                        ],
+                      ),
                       Text(
-                        _formatDate(post.date),
+                        'Post #${widget.post.id}',
                         style: TextStyle(
-                          color: Colors.grey[600],
+                          color: Colors.blue[600],
                           fontSize: 12,
+                          fontWeight: _isExpanded ? FontWeight.bold : FontWeight.normal,
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 12),
-            Text(
-              post.title,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+                ],
               ),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
             ),
-            const SizedBox(height: 8),
-            Text(
-              post.body,
-              style: TextStyle(
-                color: Colors.grey[700],
-                fontSize: 14,
-              ),
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const SizedBox(height: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Icon(Icons.favorite_border, size: 20, color: Colors.grey[600]),
-                    const SizedBox(width: 4),
-                    Text(
-                      '${(post.id * 3) % 100}',
-                      style: TextStyle(color: Colors.grey[600]),
-                    ),
-                  ],
-                ),
-                Text(
-                  'Post #${post.id}',
-                  style: TextStyle(
-                    color: Colors.blue[600],
-                    fontSize: 12,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
